fe133cabdb7d434b914ea76ad6c7de07bd60d762
dnny60 <dnny60@gmail.com>
2020-01-19 13:34:45 +0800

    asdasd

---
+++ src/components/Fish.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+
+
+class Fish extends React.Component {
+    render() {
+        return (
+            <li className='menu-fish'>
+                <img src={this.props.details.image} alt={this.props.details.name} />
+            </li>
+        )
+    }
+}
+
+
+export default Fish;
